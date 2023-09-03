00a692779d6949a0cd9041539bd77a59b51c263a
d.hripunov <ipunov@def.team>
2023-09-03 16:12:15 +0300

    upd: passport github

--- backend/routers/loginByPassport.js
+++ backend/routers/loginByPassport.js
@@ -70,9 +70,9 @@ loginByPassport.route('/auth/github/callback').get(passport.authenticate('github
     res.status(200).cookie('sessionId', sessionId, {
       httpOnly: true,
       expires: 0,
-    }).redirect('dashboard');
+    }).redirect('/');
   } catch (e) {
-    res.status(500).send(`<pre>${e}</pre>`)
+    res.redirect('/')
   }
 });
 
